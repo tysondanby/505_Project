bcff91d8b33d94e8f97675f94a759dca57eb49ea
Tyson Danby <tyson.danby@hotmail.com>
2023-12-06 14:37:31 -0700

    Fixed Graphics Bug

--- 505proj.jl
+++ 505proj.jl
@@ -3,19 +3,20 @@ using Plots,SpecialFunctions, Roots, ForwardDiff
 R1 = 1
 R2 = 2
 nu = .1
-tspan = (0,1)
-nframes = 60
+tspan = (0,3)
+nframes = 90
 number_eigenvalues = 30
-integralresolution = 5000
-Rresolution = 100
+integralresolution = 10000
+Rresolution = 250
 FPS = 30
 threedfilename = "3Danimation.gif"
 twodfilename = "2Danimation.gif"
-zmax = 0.5
-zmin = 0.0
+zmax = 0.6
+maxv = 0.2 #Max Vth value expected (for colors)
+zmin = -0.25
 
 function omega(t)
-    return .1*t
+    return 0.1*sin(2*pi*t)#.1*t
 end
 
 #Test Function
@@ -31,7 +32,7 @@ function plotanulus(rs,vs)
     #PARAMS
     resolution = 30
     coldv = zmin
-    hotv = zmax
+    hotv = maxv
     #
     ths = collect(0:2*pi/resolution:2*pi)
     xs = []
@@ -41,11 +42,19 @@ function plotanulus(rs,vs)
     top = zmax#maximum(vs) + 1*(maximum(vs)-minimum(vs))
     bot = zmin#minimum(vs) - 1*(maximum(vs)-minimum(vs))
     zrange = (bot,top)
-    p=plot(xs,ys,fs,xlims = lims,ylims = lims,zlims = zrange,linecolor = RGBA(1,0,0,1),legend = false)
+    p=plot(xs,ys,fs,xlims = lims,ylims = lims,zlims = zrange,linecolor = RGBA(1,0,0,1),legend = false,camera = (15,50))
 
     #Plot rings
     for i = 1:1:length(rs)
-        colorR = (vs[i]-coldv)/(hotv-coldv)
+        value = vs[i]
+        if (vs[i] >= 0) && (vs[i] <= hotv)
+        elseif vs[i] < 0
+            value = 0
+        else
+            value = hotv
+            #println(vs[i])
+        end
+        colorR = (value-coldv)/(hotv-coldv)
         colorB = 1-colorR
         newxs = @.sin(ths)*rs[i]
         newys = @.cos(ths)*rs[i]
